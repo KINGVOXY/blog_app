ee37cb6dbf4d73da0e03f8b47f518103cd30e23e
KINGVOXY <staichi2000@gmail.com>
2020-07-23 00:36:57 +0900

    edited route.rb & added controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,21 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get "/" => "home#top"
-  get "/about" => "home#about"
+
+  root 'home#top' # topページ
+  get '/about' => 'home#about' # 自己紹介
+
+  "
+  get '/blogs' => 'blogs#index' # 投稿一覧
+  get '/blogs/new' => 'blogs#new' # 記事投稿
+  post '/blogs/complete' => 'blogs#create' # 投稿完了処理
+
+  delete '/blog/:id/destroy' => 'blogs#destroy' # 記事削除
+
+  get    '/login' => 'sessions#new' # ログインページ
+  post   '/login' => 'sessions#create' # ログイン処理
+  delete '/logout' => 'sessions#destroy' # ログアウト処理
+
+  resources :users
+  "
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
