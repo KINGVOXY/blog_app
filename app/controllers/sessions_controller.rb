0a58b6731e323565a65e95d6563ec75c45ffa333
KINGVOXY <staichi2000@gmail.com>
2020-07-24 00:46:47 +0900

    add comment

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,17 +4,18 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password]) # ユーザーログイン後にブログ編集ができる
-      log_in user
-      redirect_to "/blog"
+    if user && user.authenticate(params[:session][:password])
+      # ユーザーログイン後にブログ編集ができる
+      log_in(user) # ログイン判定
+      redirect_to "/blogs" # リダイレクト
     else
       flash[:danger] = 'メールアドレス、パスワードのどちらかまたは両方の入力に誤りがあります。' # エラーメッセージ
-      render 'new'
+      render '/sessions/new' # loginページ再読み込み
     end
   end
 
   def destroy
-    log_out
-    redirect_to "/blog"
+    log_out # logout処理
+    redirect_to "/blogs" # 記事一覧へ
   end
 end
