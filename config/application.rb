0999051b7f2232c425c5c18718764b2808a33338
KINGVOXY <staichi2000@gmail.com>
2020-07-24 00:35:42 +0900

    set timezone

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,6 @@ module BlogApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.time_zone = 'Tokyo' # タイムゾーン設定
   end
 end
