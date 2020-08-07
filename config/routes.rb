10a77799c453ead8ee430f424f74dbf043791f16
KINGVOXY <staichi2000@gmail.com>
2020-08-08 01:14:12 +0900

    編集と更新のルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,14 @@ Rails.application.routes.draw do
   get '/blogs/new' => 'blogs#new' # 記事投稿
   post '/blogs/complete' => 'blogs#create' # 投稿完了処理
 
+  get '/blogs/:id/edit' => 'blogs#edit' # 記事編集
+  put '/blogs/:id/update' => 'blogs#update' # 記事更新
+
   delete '/blogs/:id/destroy' => 'blogs#destroy' # 記事削除
 
   get    '/login' => 'sessions#new' # ログインページ
   post   '/login' => 'sessions#create' # ログイン処理
   delete '/logout' => 'sessions#destroy' # ログアウト処理
-
-  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
